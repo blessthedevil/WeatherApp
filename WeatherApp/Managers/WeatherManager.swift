22517cd3e394cecb04653de0ad3a34e48ffc325e
blessthedevil <blessthedevil666@gmail.com>
2023-11-21 23:53:16 +0400

    Create weather and location managers

---
+++ WeatherApp/Managers/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  WeatherApp
+//
+//  Created by Максим Шишлов on 21.11.2023.
+//
+
+import Foundation
---
+++ WeatherApp/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  WeatherApp
+//
+//  Created by Максим Шишлов on 21.11.2023.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
