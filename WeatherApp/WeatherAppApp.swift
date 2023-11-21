1817a484e94b7597939eec29f99085448e34ffe5
blessthedevil <blessthedevil666@gmail.com>
2023-11-21 22:15:49 +0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Максим Шишлов on 21.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
